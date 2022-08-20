feb3e034dfaa1cedd724a4cca5250bdfbd165569
Cianenry Danan <cianenryd@gmail.com>
2022-08-20 14:59:42 -0700

    Finished multiplyArray

--- js/app.js
+++ js/app.js
@@ -99,11 +99,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-
+  let newProduct = 1;
+  for(let i = 0; i < multArr.length; i++){
+    newProduct = multiply(multArr[i], newProduct)[0];
+  }
+  return([newProduct, 'The numbers '+multArr+' have a product of '+newProduct+'.']);
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 
